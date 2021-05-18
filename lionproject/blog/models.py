95b5c46db7b6e6122b3f120611e091c3374b2dc5
minju <mindori1019@gmail.com>
2021-05-18 19:57:20 +0900

    assignment3

--- lionproject/blog/models.py
+++ lionproject/blog/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField()
-    body = models.TextField(max_length=100)
+    body = models.TextField()
+
+    
+    def __str__(self):
+        return self.title
 
 
-
+    def summary(self):
+        return self.body[:100]
--- lionproject/lionproject/urls.py
+++ lionproject/lionproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home, name="home"),
+    path('<str:id>', detail, name="detail")
 ]
