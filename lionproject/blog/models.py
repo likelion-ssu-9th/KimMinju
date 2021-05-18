a73a0a68a1396d810e004de5f02a12d99d393aed
minju <mindori1019@gmail.com>
2021-05-18 18:53:18 +0900

    setting environment for django

---
+++ lionproject/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField(max_length=100)
+
+
+
